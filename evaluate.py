fbfe68ab1a3f40e69af5f9da62c3690c4c4a4c0a
“sun” <sunyw0108@hotmail.com>
2020-06-03 20:09:20 +0200

    initial commit

---
+++ evaluate.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import os.path as osp
+
+import fcn
+import numpy as np
+import skimage.io
+import torch
+from torch.autograd import Variable
+import torchfcn
+import tqdm
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model_file', help='Model path')
+    parser.add_argument('-g', '--gpu', type=int, default=0)
+    args = parser.parse_args()
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
+    model_file = args.model_file
+
+    root = osp.expanduser('~/data/datasets')
+    val_loader = torch.utils.data.DataLoader(
+        torchfcn.datasets.VOC2011ClassSeg(
+            root, split='seg11valid', transform=True),
+        batch_size=1, shuffle=False,
+        num_workers=4, pin_memory=True)
+
+    n_class = len(val_loader.dataset.class_names)
+
+    if osp.basename(model_file).startswith('fcn32s'):
+        model = torchfcn.models.FCN32s(n_class=21)
+    elif osp.basename(model_file).startswith('fcn16s'):
+        model = torchfcn.models.FCN16s(n_class=21)
+    elif osp.basename(model_file).startswith('fcn8s'):
+        if osp.basename(model_file).startswith('fcn8s-atonce'):
+            model = torchfcn.models.FCN8sAtOnce(n_class=21)
+        else:
+            model = torchfcn.models.FCN8s(n_class=21)
+    else:
+        raise ValueError
+    if torch.cuda.is_available():
+        model = model.cuda()
+    print('==> Loading %s model file: %s' %
+          (model.__class__.__name__, model_file))
+    model_data = torch.load(model_file)
+    try:
+        model.load_state_dict(model_data)
+    except Exception:
+        model.load_state_dict(model_data['model_state_dict'])
+    model.eval()
+
+    print('==> Evaluating with VOC2011ClassSeg seg11valid')
+    visualizations = []
+    label_trues, label_preds = [], []
+    for batch_idx, (data, target) in tqdm.tqdm(enumerate(val_loader),
+                                               total=len(val_loader),
+                                               ncols=80, leave=False):
+        if torch.cuda.is_available():
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        score = model(data)
+
+        imgs = data.data.cpu()
+        lbl_pred = score.data.max(1)[1].cpu().numpy()[:, :, :]
+        lbl_true = target.data.cpu()
+        for img, lt, lp in zip(imgs, lbl_true, lbl_pred):
+            img, lt = val_loader.dataset.untransform(img, lt)
+            label_trues.append(lt)
+            label_preds.append(lp)
+            if len(visualizations) < 9:
+                viz = fcn.utils.visualize_segmentation(
+                    lbl_pred=lp, lbl_true=lt, img=img, n_class=n_class,
+                    label_names=val_loader.dataset.class_names)
+                visualizations.append(viz)
+    metrics = torchfcn.utils.label_accuracy_score(
+        label_trues, label_preds, n_class=n_class)
+    metrics = np.array(metrics)
+    metrics *= 100
+    print('''\
+Accuracy: {0}
+Accuracy Class: {1}
+Mean IU: {2}
+FWAV Accuracy: {3}'''.format(*metrics))
+
+    viz = fcn.utils.get_tile_image(visualizations)
+    skimage.io.imsave('viz_evaluate.png', viz)
+
+
+if __name__ == '__main__':
+    main()
---
+++ train_fcn32s.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+
+import argparse
+import datetime
+import os
+import os.path as osp
+import shlex
+import subprocess
+
+import torch
+import yaml
+
+import torchfcn
+
+
+def git_hash():
+    cmd = 'git log -n 1 --pretty="%h"'
+    ret = subprocess.check_output(shlex.split(cmd)).strip()
+    if isinstance(ret, bytes):
+        ret = ret.decode()
+    return ret
+
+
+def get_parameters(model, bias=False):
+    import torch.nn as nn
+    modules_skipped = (
+        nn.ReLU,
+        nn.MaxPool2d,
+        nn.Dropout2d,
+        nn.Sequential,
+        torchfcn.models.FCN32s,
+        torchfcn.models.FCN16s,
+        torchfcn.models.FCN8s,
+    )
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            if bias:
+                yield m.bias
+            else:
+                yield m.weight
+        elif isinstance(m, nn.ConvTranspose2d):
+            # weight is frozen because it is just a bilinear upsampling
+            if bias:
+                assert m.bias is None
+        elif isinstance(m, modules_skipped):
+            continue
+        else:
+            raise ValueError('Unexpected module: %s' % str(m))
+
+
+here = osp.dirname(osp.abspath(__file__))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument('-g', '--gpu', type=int, required=True, help='gpu id')
+    parser.add_argument('--resume', help='checkpoint path')
+    # configurations (same configuration as original work)
+    # https://github.com/shelhamer/fcn.berkeleyvision.org
+    parser.add_argument(
+        '--max-iteration', type=int, default=100000, help='max iteration'
+    )
+    parser.add_argument(
+        '--lr', type=float, default=1.0e-10, help='learning rate',
+    )
+    parser.add_argument(
+        '--weight-decay', type=float, default=0.0005, help='weight decay',
+    )
+    parser.add_argument(
+        '--momentum', type=float, default=0.99, help='momentum',
+    )
+    args = parser.parse_args()
+
+    args.model = 'FCN32s'
+    args.git_hash = git_hash()
+
+    now = datetime.datetime.now()
+    args.out = osp.join(here, 'logs', now.strftime('%Y%m%d_%H%M%S.%f'))
+
+    os.makedirs(args.out)
+    with open(osp.join(args.out, 'config.yaml'), 'w') as f:
+        yaml.safe_dump(args.__dict__, f, default_flow_style=False)
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
+    cuda = torch.cuda.is_available()
+
+    torch.manual_seed(1337)
+    if cuda:
+        torch.cuda.manual_seed(1337)
+
+    # 1. dataset
+
+    root = osp.expanduser('~/data/datasets')
+    kwargs = {'num_workers': 4, 'pin_memory': True} if cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        torchfcn.datasets.SBDClassSeg(root, split='train', transform=True),
+        batch_size=1, shuffle=True, **kwargs)
+    val_loader = torch.utils.data.DataLoader(
+        torchfcn.datasets.VOC2011ClassSeg(
+            root, split='seg11valid', transform=True),
+        batch_size=1, shuffle=False, **kwargs)
+
+    # 2. model
+
+    model = torchfcn.models.FCN32s(n_class=21)
+    start_epoch = 0
+    start_iteration = 0
+    if args.resume:
+        checkpoint = torch.load(args.resume)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        start_epoch = checkpoint['epoch']
+        start_iteration = checkpoint['iteration']
+    else:
+        vgg16 = torchfcn.models.VGG16(pretrained=True)
+        model.copy_params_from_vgg16(vgg16)
+    if cuda:
+        model = model.cuda()
+
+    # 3. optimizer
+
+    optim = torch.optim.SGD(
+        [
+            {'params': get_parameters(model, bias=False)},
+            {'params': get_parameters(model, bias=True),
+             'lr': args.lr * 2, 'weight_decay': 0},
+        ],
+        lr=args.lr,
+        momentum=args.momentum,
+        weight_decay=args.weight_decay)
+    if args.resume:
+        optim.load_state_dict(checkpoint['optim_state_dict'])
+
+    trainer = torchfcn.Trainer(
+        cuda=cuda,
+        model=model,
+        optimizer=optim,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        out=args.out,
+        max_iter=args.max_iteration,
+        interval_validate=4000,
+    )
+    trainer.epoch = start_epoch
+    trainer.iteration = start_iteration
+    trainer.train()
+
+
+if __name__ == '__main__':
+    main()
